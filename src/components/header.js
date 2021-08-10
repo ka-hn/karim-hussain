95b6285f3f469ed3fbd655c4d16576632bfbb0a3
Karim Hussain <hasham90@hotmail.com>
2021-08-10 11:20:43 +0200

    Update header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,12 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
@@ -28,11 +20,8 @@ export default function Header({ siteTitle }) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            { siteTitle }
+          <Typography variant="h6" className={classes.title} align="center">
+            <b>{siteTitle}</b>
           </Typography>
         </Toolbar>
       </AppBar>
