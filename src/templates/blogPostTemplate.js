4eafc260e3e087a52d1dcbd6f2c0b29363105a87
Karim Hussain <hasham90@hotmail.com>
2021-08-10 11:17:10 +0200

    Apply linting rules and remove layout wrapper

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -1,28 +1,22 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function Template ({
-  data,
-}) {
-  const { markdownRemark } = data 
-  const { frontmatter, html } = markdownRemark
+export default function Template({ data }) {
+  const { markdownRemark } = data;
+  const { frontmatter, html } = markdownRemark;
   return (
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+        <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </div>
-  )
+  );
 }
 
-
 export const pageQuery = graphql`
-  query($slug: String!) {
+  query ($slug: String!) {
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
       frontmatter {
@@ -32,4 +26,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
